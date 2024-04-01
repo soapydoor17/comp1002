d48c0425f486074b40b5670e336289a34e9dc8c1
soapydoor17 <scdonly@gmail.com>
2024-04-01 16:05:23 +0800

    Prac 3 Finished

---
+++ 03 - Stacks and Queues/EquationSolver.py
@@ -0,0 +1,121 @@
+# Activity 3 of Prac 3
+# EQUATION SOLVER: Creation of functions that solve equations
+
+import StacksAndQueues as sq
+
+class EquationError(Exception):
+    ''' Exception raise when solving equation doesn't work
+    '''
+    pass
+
+def _precedenceOf(operator):
+    ''' Private Function that determines the order of precedence of the operators
+    operator - string that is either +, -, * or /
+    Function used in _parseInfixToPostfix()
+
+    Returns 1 if operator is + or -
+    Returns 2 if operator is * or /
+    '''
+    if (operator == '+') or (operator == '-'):
+        return 1
+    elif (operator == '*') or (operator =='/'):
+        return 2
+
+def _parseInfixToPostfix(equation):
+    ''' Private Function that converts infix input from user to a postfix queue
+    equation - string from user input
+    Function used in solve()
+
+    Returns a CircularQueue
+    '''
+    infixList = equation.split()
+    postfixQueue = sq.CircularQueue()
+    operatorStack = sq.DSAStack()
+    operators = ['+', '-', '*', '/']
+
+    for i in range(len(infixList)):
+        term = infixList[i]
+        if (term == '('):
+            # start new sub equation
+            operatorStack.push('(')
+
+        elif (term ==')'):
+            # end sub equation
+            while (operatorStack.top() != '('):
+                postfixQueue.enqueue(operatorStack.pop())
+            operatorStack.pop()                               # removes '('
+
+        elif any(term == o for o in operators):
+            # place operators in postfix queue accordingly
+            while ((not operatorStack.isEmpty()) and (operatorStack.top() != '(') and
+                   (_precedenceOf(operatorStack.top()) >= _precedenceOf(term))):
+                postfixQueue.enqueue(operatorStack.pop())
+            operatorStack.push(term)
+
+        else:
+            # places numbers into postfixqueue
+            postfixQueue.enqueue(float(term))
+
+    while (not operatorStack.isEmpty()):
+        # places remaining operators into queue
+        postfixQueue.enqueue(operatorStack.pop())
+
+    return postfixQueue
+
+def _executeOperation(op, op1, op2):
+    ''' Private Function that solves a binary opertation
+    Function used in _evaluatePostfix()
+
+    op - operater (string)
+    op1 - operand to the left of operator (float)
+    op2 - operand to the right of operator (float)
+
+    Returns solution in the form of a float
+    '''
+    if op == '+':
+        return op1 + op2
+    elif op == '-':
+        return op1 - op2
+    elif op == '*':
+        return op1 * op2
+    elif op == '/':
+        return op1 / op2
+
+def _evaluatePostfix(postfixQueue):
+    ''' Private Function that determines the solution to a postfix equation
+    Function used in solve()
+
+    postfixQueue - a CircularQueue of the postfix equation
+
+    Returns the answer to the equation in the form of a float
+    '''
+    operandStack = sq.DSAStack()
+
+    while not postfixQueue.isEmpty():
+        term = postfixQueue.dequeue()
+
+        if isinstance(term, float):
+            operandStack.push(term)
+
+        elif isinstance(term, str):
+            op2 = operandStack.pop()
+            op1 = operandStack.pop()
+            binSoln = _executeOperation(term, op1, op2)
+            operandStack.push(binSoln)
+
+    if operandStack.getCount() == 1:
+        return operandStack.top()
+    else:
+        raise EquationError('More than one term left over, input may not be formatted properly')
+
+def solve(equation):
+    ''' Public Function that solves an equation
+
+    equation - string of equation to be solved
+
+    Returns answer in the form of a float
+    '''
+    postfixQueue = _parseInfixToPostfix(equation)
+    answer = _evaluatePostfix(postfixQueue)
+
+    return answer
---
+++ 03 - Stacks and Queues/StacksAndQueues.py
@@ -0,0 +1,184 @@
+# Activity 1 and 2 of Prac 3
+# Implementation of Stacks and Queues using arrays as the data structure
+# Contains code for DSAStack, DSAQueue, ShufflingQueue and CircularQueue
+# Also contains custom exceptions for when ADTs are full or empty
+
+import numpy
+
+# EXCEPTION INITIALISATION
+class EmptyException(Exception):
+    ''' Error attempting to access an element from an empty container
+    '''
+    pass
+
+class FullException(Exception):
+    ''' Error attempting to access an element from a full container
+    '''
+    pass
+
+# CLASS INITIALISATION
+class DSAStack():
+    def __init__(self, maxCapacity=100):
+        ''' Intialisation
+
+        maxCapacity - initialises number of elements in stack (default 100)
+        
+        self.stack - object array with LIFO functionality
+        self.capacity - how many elements the stack is able to hold
+        self.count - the number of elements currently in the stack
+        '''
+        self.stack = numpy.empty(maxCapacity, dtype=object)
+        self.capacity = maxCapacity
+        self.count = 0
+    
+    def getCount(self):
+        ''' ACCESSOR: Returns number (int) of elements in the stack
+        '''
+        return self.count
+    
+    def isEmpty(self):
+        ''' ACCESSOR: Returns boolean that is true if there are currently no elements in the stack
+        '''
+        empty = (self.count == 0)
+        return empty
+    
+    def isFull(self):
+        ''' ACCESSOR: Returns boolean that is true if the stack is at capacity
+        '''
+        full = (self.count == self.capacity)
+        return full
+
+    def top(self):
+        ''' ACCESSOR: Looks at top-most item but leaves it in the stack
+        '''
+        if self.isEmpty():
+            raise EmptyException('Stack is empty')
+        else:
+            topVal = self.stack[self.count-1]
+            return topVal
+        
+    def push(self, obj):
+        ''' MUTATOR: Adds a new element to the top of the stack
+        
+        obj - element to be added to stack
+        '''
+        if self.isFull():
+            raise FullException('Stack is full')
+        else:
+            self.stack[self.count] = obj
+            self.count += 1
+
+    def pop(self):
+        ''' MUTATOR: takes off top most element of stack and returns it
+        '''
+        topVal = self.top()
+        self.count -= 1
+        return topVal
+
+
+class DSAQueue():
+    def __init__(self, maxCapacity=100):
+        ''' Intialisation
+
+        maxCapacity - initialises number of elements in queue (default 100)
+        
+        self.queue - object array with FIFO functionality
+        self.capacity - how many elements the queue is able to hold
+        self.count - the number of elements currently in the queue
+        '''
+        self.queue = numpy.empty(maxCapacity, dtype=object)
+        self.capacity = maxCapacity
+        self.count = 0
+
+    def getCount(self):
+        ''' ACCESSOR: Returns number (int) of elements in the queue
+        '''
+        return self.count
+    
+    def isEmpty(self):
+        ''' ACCESSOR: Returns boolean that is true if there are currently no elements in the queue
+        '''
+        empty = (self.count == 0)
+        return empty
+    
+    def isFull(self):
+        ''' ACCESSOR: Returns boolean that is true if the queue is at capacity
+        '''
+        full = (self.count == self.capacity)
+        return full
+    
+    def peek(self):
+        ''' ACCESSOR: Looks at last item but leaves it in the stack
+        '''
+        if self.isEmpty():
+            raise EmptyException('Queue is empty')
+        else:
+            frontVal = self.queue[0]
+            return frontVal
+        
+    def enqueue(self, obj):
+        ''' MUTATOR: adds to the end of the queue
+        
+        obj - element to be added to the queue
+        '''
+        if self.isFull():
+            raise FullException('Queue is full')
+        else:
+            self.queue[self.count] = obj
+            self.count += 1
+
+    def dequeue(self):
+        ''' MUTATOR: Returns value from front of queue and removes it, shuffling down all other items
+        '''
+        frontVal = self.peek()
+        for i in range(1, len(self.queue)):
+            self.queue[i-1] = self.queue[i]
+        self.count -= 1
+        return frontVal
+
+class ShufflingQueue(DSAQueue):
+    pass
+
+class CircularQueue(DSAQueue):
+    def __init__(self, maxCapacity=100):
+        ''' INITIALISATION - inherits from DSAQueue
+
+        self.front - index of where the first element is
+        '''
+        super().__init__(maxCapacity)
+        self.front = 0
+
+    def peek(self):
+        ''' ACCESSOR: Looks at last item but leaves it in the stack
+        '''
+        if self.isEmpty():
+            raise EmptyException('Queue is empty')
+        else:
+            frontVal = self.queue[self.front]
+            return frontVal
+        
+    def enqueue(self, obj):
+        ''' MUTATOR: adds to the end of the queue
+        
+        obj - element to be added to the queue
+        '''
+        if self.isFull():
+            raise FullException('Queue is full')
+        else:
+            index = (self.front + self.count) % len(self.queue)  # modulus accounts for overflow
+            self.queue[index] = obj
+            self.count += 1
+
+    def dequeue(self):
+        ''' MUTATOR: Returns value from front of queue and removes it, changing the location of the front of the queue
+        '''
+        frontVal = self.peek()
+        self.count -= 1
+        
+        # Return self.front to zero if it is going to overflow
+        if self.front == self.capacity - 1:
+            self.front = 0
+        else:
+            self.front += 1
+
+        return frontVal
